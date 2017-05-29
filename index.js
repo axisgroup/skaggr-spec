e16185c8c4c6d166b5a741fdbc7494fe145657e4
Speros Kokenes <speroskokenes@gmail.com>
2017-05-28 22:56:00 -0400

    added index and fixed group typo

---
+++ index.js
@@ -0,0 +1,11 @@
+module.exports = {
+    aggr: require("./types/aggr.json"),
+    explicit: require("./types/explicit.json"),
+    function: require("./types/function.json"),
+    group: require("./types/group.json"),
+    qualifier: require("./types/qualifier.json"),
+    "set-component": require("./types/set-component.json"),
+    "set-field": require("./types/set-field.json"),
+    "set-list": require("./types/set-list.json"),
+    "set-modifier": require("./types/set-modifier.json")
+}
